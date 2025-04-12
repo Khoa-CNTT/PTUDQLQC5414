bee7451a57ad77ea3e4425ecee989d3e6697d03c
minhle0501 <SUNNYDN12345678@GMAIL.COM>
2025-04-12 14:51:09 +0700

    add form review-switch tab review add product info

---
+++ backend/models/reviewModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema(
+  {
+    productId: { type: String, required: true },
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+export default Review;
